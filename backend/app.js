2d7dbf275ae4555c6209820c53f6a11ee84ffcd2
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2023-07-20 00:28:51 +0300

    feat: добавить анализатор логов

--- backend/app.js
+++ backend/app.js
@@ -5,16 +5,17 @@ const cors = require('cors');
 const { errors, celebrate, Joi } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/NotFoundError');
 
 const { PORT = 3000, DB_CONNECT_ADDRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 mongoose
-	.connect(DB_CONNECT_ADDRESS, {
-		useNewUrlParser: true
-	})
-	.then(() => console.log('connected to db'))
-	.catch(err => console.log(`Ошибка: ${err.message}`));
+  .connect(DB_CONNECT_ADDRESS, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('connected to db'))
+  .catch(err => console.log(`Ошибка: ${err.message}`));
 
 const app = express();
 
@@ -22,51 +23,54 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
+app.use(requestLogger);
 
 app.use('/users', auth, require('./routes/users'));
 app.use('/cards', auth, require('./routes/cards'));
 
 app.post(
-	'/signin',
-	celebrate({
-		body: Joi.object().keys({
-			email: Joi.string()
-				.required()
-				.pattern(/\w+\@\w+\.\w+/),
-			password: Joi.string().required()
-		})
-	}),
-	login
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string()
+        .required()
+        .pattern(/\w+\@\w+\.\w+/),
+      password: Joi.string().required()
+    })
+  }),
+  login
 );
 app.post(
-	'/signup',
-	celebrate({
-		body: Joi.object().keys({
-			email: Joi.string()
-				.required()
-				.pattern(/\w+\@\w+\.\w+/),
-			password: Joi.string().required(),
-			name: Joi.string().min(2).max(30),
-			about: Joi.string().min(2).max(30),
-			avatar: Joi.string().pattern(
-				/^((http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?/
-			)
-		})
-	}),
-	createUser
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string()
+        .required()
+        .pattern(/\w+\@\w+\.\w+/),
+      password: Joi.string().required(),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(
+        /^((http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?/
+      )
+    })
+  }),
+  createUser
 );
 
 app.use((req, res, next) => {
-	next(new NotFoundError('Ресурс не найден'));
+  next(new NotFoundError('Ресурс не найден'));
 });
+
+app.use(errorLogger);
 
 app.use(errors());
 
 app.use((err, req, res, next) => {
-	if (err && err.statusCode) res.status(err.statusCode).send({ message: err.message });
-	else res.status(500).send({ message: 'На сервере произошла ошибка' });
+  if (err && err.statusCode) res.status(err.statusCode).send({ message: err.message });
+  else res.status(500).send({ message: 'На сервере произошла ошибка' });
 });
 
 app.listen(PORT, () => {
-	console.log(`server is running on port ${PORT}`);
+  console.log(`server is running on port ${PORT}`);
 });
